01ba5b436c4620f32876cd606b36e4a3d3b6071f
TakamichiTatsumoto <t.t.tatsumoto@gmail.com>
2020-07-28 07:43:46 +0900

    add RequestMapping

--- src/main/java/com/example/helloSpring/HelloSpringApplication.java
+++ src/main/java/com/example/helloSpring/HelloSpringApplication.java
@@ -3,14 +3,16 @@ package com.example.helloSpring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/")
 public class HelloSpringApplication {
 
 	@GetMapping
-	String home() {
+	String getHelloMessage() {
 		return "Hello Spring!";
 	}
 
